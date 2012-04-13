34f5be358c81769b4458e5918775ff63a0ce92fe
Jan Hermann <honza.hermann@gmail.com>
2012-04-13 22:21:16 +0200

    bfgs

--- core/updatehessian.m
+++ core/updatehessian.m
@@ -1,11 +1,11 @@
 % updates Hessian. 12/04/12
 
 function H = updatehessian(H,dq,dg)
-	%BFGS = dg*dg'/(dq'*dg)-H*(dq*dq')*H/(dq'*H*dq); % BFGS update
-	MS = (dg-H*dq)*(dg-H*dq)'/(dq'*(dg-H*dq)); % MS update
+	BFGS = dg*dg'/(dq'*dg)-H*(dq*dq')*H/(dq'*H*dq); % BFGS update
+	%MS = (dg-H*dq)*(dg-H*dq)'/(dq'*(dg-H*dq)); % MS update
 	%Q = abs(dq'*(dg-H*dq))/(norm(dq)*norm(dg-H*dq)); % Bofill's coeff
 	%dH = (1-Q)*BFGS+Q*MS;
-	dH = MS; % full MS update
+	dH = BFGS;
 	H = H+dH;
 	print('Hessian update information:');
 	%print('* Bofill''s coefficient: %.3g',Q);
