7a4cdfb963dd067850aff8ed3190026870f74b8b
Jan Hermann <honza.hermann@gmail.com>
2012-04-17 23:20:34 +0200

    bugfixes, tuning

--- readwrite/print.m
+++ readwrite/print.m
@@ -2,16 +2,19 @@
 
 function print(s,varargin)
 	global fid steps
-	s = ['%i ' s '\n'];
 	if nargin > 1 && strcmp(varargin{end},'always')
 		% if last argument is 'always', print anyway
 		varargin(end) = [];
-		fprintf(1,s,steps,varargin{:});
+		fprintf(1,'%i ',steps)
+		fprintf(1,s,varargin{:});
+		fprintf(1,'\n');
 		octfflush(1);
 	end
 	if fid > 0
 		fseek(fid,0,'eof');
-		fprintf(fid,s,steps,varargin{:});
+		fprintf(fid,'%i ',steps)
+		fprintf(fid,s,varargin{:});
+		fprintf(fid,'\n');
 		octfflush(fid);
 	end
 end
